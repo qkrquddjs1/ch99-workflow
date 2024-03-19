36d469d7e0043be75aec9ffe8257735837420994
1sol2 <lhs1119r@gmail.com>
2024-03-19 14:54:58 +0900

    com

---
+++ src/main/java/com/ohgiraffers/ch99workflow/Ch99WorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.ch99workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch99WorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch99WorkflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohgiraffers/ch99workflow/Ch99WorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.ch99workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch99WorkflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
